77f6bf6619b1b5694836fb8bf152bd3d15c57df5
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2019-10-02 02:36:37 +0000

    Create launcher.sh

---
+++ .config/polybar/Scripts/launcher.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Jogar na pasta: ~/.config/polybar/Scripts
+
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar -q top &
+polybar -q bottom &
