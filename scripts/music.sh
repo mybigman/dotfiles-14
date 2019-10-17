6c322ca2695d6843bfbea0eed8919cbd1d8a2748
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2019-10-17 13:38:19 +0000

    Create music.sh

---
+++ scripts/music.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Um script pra mostrar a notificação e capa do álbm da sua música 
+#
+# Criador: Jairo Abreu, Telegram: @jairoabreeu
+#
+# Credits: Franklin Souza, Telegram: @FranklinTech
+while true
+do
+    mpc idle player
+
+    NOTIFY_TITLE="Tocando Agora"
+
+    MUSIC=$(mpc current)
+
+    echo "$MUSIC"
+
+    ART="$HOME/Música/$(dirname "$(mpc status -f '%file%' | head -n1)")/cover.jpg"
+    convert -resize 64x64 "$ART" /tmp/cover.png
+
+    dunstify -u low -t 5000 -a MPD -r 1 -i "/tmp/cover.png" "$NOTIFY_TITLE"  "$MUSIC"
+done
