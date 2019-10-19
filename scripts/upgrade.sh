96602538326bedc60b0cc9f27605256275eccbd2
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2019-10-19 13:50:41 +0000

    Update upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-echo
+#!/bin/sh
+#echo Inf atual: date +%a %d/%b - %H:%M:%S
 echo "By: Franklin Souza"
 echo
 echo "By: @FranklinTech"
 echo
-echo "Atualizar Arch Linux"
+echo "Atualizar Arch Linux e AUR"
 echo
 echo "Deseja atualiza seu Arch Linux agora?"
 echo
@@ -15,12 +15,12 @@ echo
 read ALT
 echo
 if [ "$ALT" = "1" ];then
-	echo "Seu Arch Linux irá atualizar...AGUARDE"
+	echo "Seu Arch Linux e AUR irão atualizar...AGUARDE"
 	sleep 1
 	echo
 	echo "Digite sua senha SUDO se pedir:"
 	echo
-	sudo pacman -Syu
+	yay -Syyuu 
 	echo
 	echo "Arch Linux atualizado com sucesso...SAINDO!!!"
 	sleep 2 && exit
