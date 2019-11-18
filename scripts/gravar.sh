24930ad80d6a874ff2d17847dda7a592433c5760
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2019-11-18 09:03:14 -0300

    Update gravar.sh

--- scripts/gravar.sh
+++ scripts/gravar.sh
@@ -2,4 +2,4 @@
 
 notify-send "FFmpeg" "Gravação inicada"
 
-ffmpeg -video_size 1600x900 -framerate 25 -f x11grab -i :0.0 -f pulse -ac 2 -i default /home/$USER/Vídeos/Tela_$(date +%d-%m-%Y_%H:%M:%S).mp4
+ffmpeg -video_size 1600x900 -framerate 25 -f x11grab -i :0.0 -f pulse -ac 2 -i default $HOME/Vídeos/Tela_$(date +%d-%m-%Y_%H:%M:%S).mp4
