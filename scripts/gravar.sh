f061024c153a4c3f20477bf16ab2dba75b4d3ec5
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2019-11-27 17:02:06 +0000

    Update gravar.sh

--- scripts/gravar.sh
+++ scripts/gravar.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+# Credit: Franklin Souza
+# Telegram: @FranklinTech
 
-notify-send "FFmpeg" "Gravação inicada"
-
-ffmpeg -video_size 1600x900 -framerate 25 -f x11grab -i :0.0 -f pulse -ac 2 -i default $HOME/Vídeos/Tela_$(date +%d-%m-%Y_%H:%M:%S).mp4
+resolucao=$(xrandr | grep '*' | awk 'NR==1{print $1}')
+if [ ! $1 ]; then
+    local="$HOME/Vídeos/"
+    nome="$(date "+%Y-%m-%d--%H-%M-%S").mp4"
+    arquivo="$local/$nome"
+fi
+if pgrep -x "ffmpeg" > /dev/null
+then
+    [ "$(pgrep -x polybar)" ] && [ "$1" == "status" ] && exit
+    if [ ! $1 ]; then
+        killall ffmpeg
+        notify-send "Gravação finalizada" "$nome"
+        exit 0
+    fi
+else
+    [ "$(pgrep -x polybar)" ] && [ "$1" == "status" ] && exit
+    if [ ! "$1" ]; then
+        notify-send "Gravação iniciada"
+        ffmpeg -f x11grab -s $resolucao -i :0.0 -f pulse -ac 2 -i default -c:v libx264 -crf 23 -profile:v baseline -level 3.0 -pix_fmt yuv420p -c:a aac -ac 2 -strict experimental -b:a 128k -movflags faststart $arquivo
+    fi
+fi
+exit 0
