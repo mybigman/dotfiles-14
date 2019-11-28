19a48b10d28d11a5afbfba32ad4b9a62bf290ea3
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2019-11-28 01:03:15 +0000

    Update gravar.sh

--- scripts/gravar.sh
+++ scripts/gravar.sh
@@ -13,13 +13,13 @@ then
     [ "$(pgrep -x polybar)" ] && [ "$1" == "status" ] && exit
     if [ ! $1 ]; then
         killall ffmpeg
-        notify-send "Gravação finalizada" "$nome"
+        notify-send "Gravação de tela finalizada!" "$nome"
         exit 0
     fi
 else
     [ "$(pgrep -x polybar)" ] && [ "$1" == "status" ] && exit
     if [ ! "$1" ]; then
-        notify-send "Gravação iniciada"
+      notify-send "FFmpeg" "Gravação da tela iniciada!"
         ffmpeg -f x11grab -s $resolucao -i :0.0 -f pulse -ac 2 -i default -c:v libx264 -crf 23 -profile:v baseline -level 3.0 -pix_fmt yuv420p -c:a aac -ac 2 -strict experimental -b:a 128k -movflags faststart $arquivo
     fi
 fi
