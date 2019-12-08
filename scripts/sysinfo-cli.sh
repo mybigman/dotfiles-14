b5d68bdbbf2b3657135c1fa2bd545158302a92ae
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2019-12-08 02:22:43 +0000

    Update sysinfo-cli.sh

--- scripts/sysinfo-cli.sh
+++ scripts/sysinfo-cli.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
 TAB=""
-PHOTO="/home/kleber/Downloads/stretched-1366-768-662283.jpg"
+PHOTO="$HOME/Franks/Wallpapers/136.png"
 COLOR="\e[1;38;05;32m"
 
 OS=$(uname -nm)
-KERNEL=$(uname -r | sed 's/-[[:alpha:]]*$//')
+#KERNEL=$(uname -r | sed 's/-[[:alpha:]]*$//')
+KERNEL=$(uname -r)
 RESOLUTION=$(xrandr | awk '/*+/{print $1}')
 PACKAGES=$(pacman -Qq | wc -l)
 
 UPTIME=$(awk '{printf("%i Hour(s), %i Minute(s)", ($1/60/60%24), ($1/60%60))}' /proc/uptime)
-CPU=$(awk -F': ' '/model name/{gsub(/\([[:alpha:]]+)|CPU([[:blank:]]{2,})?/,"");print $2}' /proc/cpuinfo)
-MEM=$(free -m | awk '/Mem/{print $3"MiB", $2"MiB"}' OFS=' / ')
+CPU=$(cat /proc/cpuinfo | sed -n '5p' | awk '{print $4, $5, $6, $7, $8, $9}')
+GPU=$(inxi -G)
+MEM=$(free -m | awk '/Mem/{print $3"MiB", $2"MiB"}'OFS=' / ')
 
 clear
 
 echo
 echo -e " ${TAB} ${COLOR}$USER@$(uname -n)\033[0m"
 echo -e " ${TAB} ----------"
-echo -e " ${TAB} ${COLOR}OS\033[0m .........: $OS"
-echo -e " ${TAB} ${COLOR}Kernel\033[0m .....: $KERNEL"
-echo -e " ${TAB} ${COLOR}Resolution\033[0m .: $RESOLUTION"
-echo -e " ${TAB} ${COLOR}Packages\033[0m ...: $PACKAGES"
+echo -e " ${TAB} ${COLOR}OS\033[0m: $OS"
+echo -e " ${TAB} ${COLOR}Kernel\033[0m: $KERNEL"
+echo -e " ${TAB} ${COLOR}Resolution\033[0m: $RESOLUTION"
+echo -e " ${TAB} ${COLOR}Packages\033[0m: $PACKAGES"
+echo -e " ${TAB} ${COLOR}Shell\033[0m: $SHELL"
 echo
-echo -e " ${TAB} ${COLOR}Uptime\033[0m .....: $UPTIME"
-echo -e " ${TAB} ${COLOR}Shell\033[0m ......: $SHELL"
-echo -e " ${TAB} ${COLOR}Cpu\033[0m ........: $CPU"
-echo -e " ${TAB} ${COLOR}Mem\033[0m ........: $MEM"
-echo
+#echo -e " ${TAB} ${COLOR}Uptime\033[0m .....: $UPTIME"
+#echo -e " ${TAB} ${COLOR}Cpu\033[0m ........: $CPU"
+#echo -e " ${TAB} ${COLOR}Cpu\033[0m ........: $GPU"
+#echo -e " ${TAB} ${COLOR}Mem\033[0m ........: $MEM"
+#echo
 echo -e " ${TAB}  \033[1;31m███\033[1;32m███\033[1;33m███\033[1;34m███\033[1;35m███\033[1;36m███"
 echo
 echo
