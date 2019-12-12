70dffecb9633ae5dd827082583ff74d022f949ca
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2019-12-12 02:54:45 +0000

    Update launcher.sh

--- .config/polybar/scripts/launcher.sh
+++ .config/polybar/scripts/launcher.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+# Credit: Franklin Souza
+# Telegra: @FranklinTech
+
+# Se quiser só a bar de baixo, comenta a (polybar -q top &) se quiser só a bar de cima comenta a (polybar -q bottom &)
+# E se quiser as duas, deixa ambas descomentas
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,5 +11,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -q top &
+#polybar -q top &
 polybar -q bottom &
