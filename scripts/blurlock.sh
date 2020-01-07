7023999ac0f0c6a7a036cc9c590af062fc307f1f
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2020-01-07 18:44:18 -0300

    Create blurlock.sh

---
+++ scripts/blurlock.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Credit: Franklin Souza
+# Telegram: @FranklinTech
+#
+# take screenshot
+import -window root /tmp/screenshot.png
+
+# blur it
+convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+rm /tmp/screenshot.png
+
+# lock the screen
+i3lock  -e -i /tmp/screenshotblur.png
+
+exit 0
