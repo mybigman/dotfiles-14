deb9d10f35c09fd93ecd09ac20e13cf65d659e7b
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2020-01-11 02:23:24 -0300

    Update ping.sh

--- .config/polybar/scripts/ping.sh
+++ .config/polybar/scripts/ping.sh
@@ -3,7 +3,7 @@
 HOST=192.168.0.1
 
 if ! ping=$(ping -n -c 1 -W 1 $HOST 2> /dev/null); then
-    echo "%{F#BF616A}%{F-} ?"
+    echo "%{F#BF616A}%{F-} Host não identificado"
 else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
