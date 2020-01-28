14a802095310157367a21229d0ab726d6062c726
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2020-01-28 13:16:19 -0300

    Create infopc.sh

---
+++ scripts/infopc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Informações do sistema
+# Kernel, CPUs, Memória e Uptime
+# -
+# Franklin Souza
+#
+# @FranklinTech
+#
+KERNEL=$(uname -r)
+
+PROCESSADOR_NOME=$(grep 'model name' /proc/cpuinfo | tail -n 1 | cut -d':' -f 2 | sed 's/^ //g')
+PROCESSADOR_CORES=$(grep 'cpu cores' /proc/cpuinfo | tail -n 1 | cut -d':' -f 2 | sed 's/^ //g')
+
+MEMORIA_TOTAL_KB=$(grep MemTotal /proc/meminfo | grep -Eo '[0-9]{1,}')
+MEMORIA_TOTAL_MB=$(echo $MEMORIA_TOTAL_KB / "1024" | bc)
+SWAP_TOTAL_KB=$(grep SwapTotal /proc/meminfo | grep -Eo '[0-9]{1,}')
+#SWAP_TOTAL_MB=$(echo $SWAP_TOTAL_KB / "1024" | bc)
+
+UPTIME_S=$(cut -d' ' -f 1 /proc/uptime)
+UPTIME_MIN=$(uptime --pretty | sed 's/up //; s/\ days\?,/d/; s/\ hours\?,\?/h/; s/\ minutes\?/m/')
+
+clear
+echo -e "+----------------------------------------------------------+"
+echo -e "|                  INFORMAÇÕES DO SISTEMA                  |"
+echo -e "+----------------------------------------------------------+"
+echo -e
+echo -e "  Distribuição.......: "Arch Linux"\n"
+echo -e "  Kernel.......: "$KERNEL"\n"
+echo -e "  Processador..: "$PROCESSADOR_NOME
+echo -e "  Cores.........: "$PROCESSADOR_CORES"\n"
+echo -e "  Memória......: "$MEMORIA_TOTAL_KB" KB"
+#echo -e "  Memória......: "$MEMORIA_TOTAL_MB" MB"
+echo -e "  Memória Swap.: "$SWAP_TOTAL_KB" KB"
+#echo -e "  Memória Swap.: "$SWAP_TOTAL_MB" MB\n"
+echo -e "  Uptime.......: "$UPTIME_MIN" minutos"
+echo -e "+----------------------------------------------------------+"
