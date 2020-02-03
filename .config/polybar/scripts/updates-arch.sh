68246ec022e765306a59e0068bbbab3be6a3f60c
Franklin Souza <51012258+fffranks@users.noreply.github.com>
2020-02-03 06:44:31 -0300

    Update updates-arch.sh

--- .config/polybar/scripts/updates-arch.sh
+++ .config/polybar/scripts/updates-arch.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+# Credit: Franklin Souza
+# Telgram: @FranklinTech
 
 if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
     updates=0
 fi
 
 if [ "$updates" -gt 0 ]; then
-    echo " $updates "
+    echo " $updates "
 else
-    echo "Sem Updates"
+    echo "  Sem atualizações"
 fi
