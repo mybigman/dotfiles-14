5cb54ad29914f37461b543f686077f7863a02595
Franklin Souza <franklinpb98@gmail.com>
2020-03-21 02:40:27 +0000

    Autocommit por GitHub.sh: alterações: add '.config/polybar/scripts/gravar.sh'

--- .config/polybar/scripts/gravar.sh
+++ .config/polybar/scripts/gravar.sh
@@ -13,14 +13,14 @@ fi
 
 if pgrep -x "ffmpeg" > /dev/null
 then
-    [ "$(pgrep -x polybar)" ] && [ "$1" == "status" ] && echo "%{F#06ff83}%{F-}" && exit
+    [ "$(pgrep -x polybar)" ] && [ "$1" == "status" ] && echo "%{F#f82c2c}%{F-}" && exit
     if [ ! $1 ]; then
         killall ffmpeg
         notify-send "FFmpeg" "Gravação de tela finalizada!"
         exit 0
     fi
 else
-    [ "$(pgrep -x polybar)" ] && [ "$1" == "status" ] && echo "" && exit
+    [ "$(pgrep -x polybar)" ] && [ "$1" == "status" ] && echo "" && exit
     if [ ! "$1" ]; then
         notify-send "FFmpeg" "Gravação de tela iniciada!"
         ffmpeg -f x11grab -s $resolucao -i :0.0 -f pulse -ac 2 -i default -c:v libx264 -crf 23 -profile:v baseline -level 3.0 -pix_fmt yuv420p -c:a aac -ac 2 -strict experimental -b:a 128k -movflags faststart $arquivo
