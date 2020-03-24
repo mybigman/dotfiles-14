7d27118ef6b0f58a807d424a06eb754f57831e97
Franklin Souza <franklinpb98@gmail.com>
2020-03-24 09:31:36 +0000

    Autocommit por GitHub.sh: alterações: add 'scripts/userbot.sh'

--- scripts/userbot.sh
+++ scripts/userbot.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 echo
+clear
 figlet UserBot FTG | lolcat
 echo
 echo
-echo "Iniciando o bot no Telegram..."
-sleep 4
-echo
-echo "Bot iniciado com sucesso!!!"
-echo
-sleep 2 && clear
-echo
 echo "BOT RODANDO..." | lolcat
+echo
+sleep 2
 echo
 cd ~/.Git/friendly-telegram && python3 -m friendly-telegram
